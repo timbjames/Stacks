bc33e123b2d466f8002a9105ae865257054f29ae
timbjames <timbjames@gmail.com>
2012-05-01 15:08:05 +0100

    added example controls

---
+++ WebApp/Controls/GridView/row-databound.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/skin/global.Master" AutoEventWireup="true" CodeBehind="row-databound.aspx.cs" Inherits="WebApp.Controls.GridView.row_databound" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <asp:GridView ID="gv1" runat="server"></asp:GridView>
+
+</asp:Content>
---
+++ WebApp/Controls/GridView/row-databound.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.Controls.GridView
+{
+    public partial class row_databound : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.gv1.DataSource = Enumerable.Range(0, 10);
+            this.gv1.RowDataBound += new GridViewRowEventHandler(gv1_RowDataBound);
+            this.gv1.DataBind();
+            
+        }
+
+        void gv1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+
+            }
+        }
+    }
+}
