0e701da3f78b2f85d71d93593ef88fd18a0a2502
timbjames <timbjames@gmail.com>
2012-05-18 13:31:58 +0100

    added mvc ActionLink helper

---
+++ Mvc4App/Helpers/HelperExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Mvc.Ajax;
+
+public static class HtmlHelperExtensions
+{
+
+    public static MvcHtmlString RawActionLink(this AjaxHelper ajaxHelper, string rawHtml, string action, string controller, object routeValues, AjaxOptions ajaxOptions, object htmlAttributes)
+    {
+        string anchor = ajaxHelper.ActionLink("##holder##", action, controller, routeValues, ajaxOptions, htmlAttributes).ToString();
+        return MvcHtmlString.Create(anchor.Replace("##holder##", rawHtml));
+    }
+
+    public static MvcHtmlString RawActionLink(this HtmlHelper htmlHelper, string rawHtml, string action, string controller, object routeValues, object htmlAttributes)
+    {
+        string anchor = htmlHelper.ActionLink("##holder##", action, controller, routeValues, htmlAttributes).ToString();
+        return MvcHtmlString.Create(anchor.Replace("##holder##", rawHtml));
+    }
+
+}
